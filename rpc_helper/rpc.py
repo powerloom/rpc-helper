34244cd4683404865980d1d6cf599def9b360bad
Seth <seth@powerloom.io>
2025-05-01 14:33:36 +0530

    Update SSL verification in RpcHelper to use explicit certificate path instead of certifi

--- rpc_helper/rpc.py
+++ rpc_helper/rpc.py
@@ -1,5 +1,4 @@
 import asyncio
-import certifi
 from sys import stdout
 from typing import Any
 from typing import List
@@ -153,7 +152,7 @@ class RpcHelper(object):
                 max_keepalive_connections=self._rpc_settings.connection_limits.max_keepalive_connections,
                 keepalive_expiry=self._rpc_settings.connection_limits.keepalive_expiry,
             ),
-            verify=certifi.where(),  # Use CA bundle from certifi
+            verify="/etc/ssl/certs/ca-certificates.crt",  # Explicitly set the certificate path
         )
         self._client = AsyncClient(
             timeout=Timeout(timeout=15.0),
